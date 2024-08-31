038c9770035ad2518f98fb0230cf2a4b43aa645c
nikhil <nikhilpalem93466@gmail.com>
2024-08-31 16:19:15 +0530

    0.2.2

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -4,17 +4,19 @@ import './Scoreboard.css';
 
 function Scoreboard({ username, score }) {
   const [scores, setScores] = useState([]);
-  const BackendUrl="https://quiz-backend-gray.vercel.app";
+  const BackendUrl = "https://quiz-backend-gray.vercel.app";
 
   console.log(username, score)
   useEffect(() => {
     const fetchScores = async () => {
       const response = await axios.get(`${BackendUrl}/api/scores`);
       setScores(response.data);
+      console.log(response.data, "data");
     };
     fetchScores();
   }, []);
 
+  console.log(scores);
   return (
     <div className="scoreboard">
       <h2>Scoreboard</h2>
@@ -26,17 +28,17 @@ function Scoreboard({ username, score }) {
           </tr>
         </thead>
         <tbody>
-            {scores.map((scoreItem, index) => (
-              <tr
-                key={index}
-                style={{
-                  fontWeight: scoreItem.username === username ? 'bold' : 'normal',
-                  color: scoreItem.username === username ? '#34a853' : 'initial',
-                }}>
-                  <td>{scoreItem.username}</td>
-                  <td>{scoreItem.score}</td>
-                </tr>
-            ))}
+          {scores.map((scoreItem, index) => (
+            <tr
+              key={index}
+              style={{
+                fontWeight: scoreItem.username === username ? 'bold' : 'normal',
+                color: scoreItem.username === username ? '#34a853' : 'initial',
+              }}>
+              <td>{scoreItem.username}</td>
+              <td>{scoreItem.score}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <h3>Your Score: {score}</h3>
